bcd7ed86dc65cd69574044ff958c768b27dffba7
esouthren <eilidhsouthren@gmail.com>
2017-10-01 20:15:28 +0100

    Add files via upload
    Comments, PEP8 standards, dynamic selection of joke to accommodate changing size of data source.

--- app.py
+++ app.py
@@ -4,18 +4,17 @@ from flask_restful import Resource, Api
 import joke
 import sys
 
-
 app = Flask(__name__)
 api = Api(app)
-
-
-
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+
 class APP(Resource):
+
     def get(self):
         return {'this is soon to become an awesome->': 'website'}
+
 
     def post(self):
         jk = joke.getJoke()
@@ -23,12 +22,15 @@ class APP(Resource):
         #jk = jk.encode('utf-8')
         return jk
 
+
 class API(Resource):
+
     def get(self):
         jk = joke.getJoke()
         jk = jk.encode('ascii', 'ignore').decode('ascii')
         # jk = jk.encode('utf-8')
         return jk
+
 
 api.add_resource(APP, '/')
 api.add_resource(API, '/api')
--- joke.py
+++ joke.py
@@ -2,13 +2,16 @@
 import json
 from random import *
 
-def random_digits():
-    range_start = 1
-    range_end = 545
-    return randint(range_start, range_end)
-def getJoke():
+
+def random_digits(joke_count):
+    # Return a joke index between first and last joke in data
+    return randint(1, joke_count)
+
+
+def get_joke():
+    # Return random joke
     with open('data.json') as data_file:
         data = json.load(data_file)
-    joke = data[random_digits()]
+    joke = data[random_digits(len(data))]
     print joke
     return joke
