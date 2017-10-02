2944de53eae3571165cbb710d839150af130315a
Earvin Gemenez <earvin.gemenez@gmail.com>
2017-10-02 13:08:47 +0800

    convert it to modular

--- app.py
+++ app.py
@@ -1,39 +1,12 @@
 # -*- coding: utf-8 -*-
 from flask import Flask
-from flask_restful import Resource, Api
-import joke
 import sys
 
-app = Flask(__name__)
-api = Api(app)
+application = Flask(__name__)
+
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-class APP(Resource):
-
-    def get(self):
-        return {'this is soon to become an awesome->': 'website'}
-
-
-    def post(self):
-        jk = joke.getJoke()
-        jk = jk.encode('ascii', 'ignore').decode('ascii')
-        #jk = jk.encode('utf-8')
-        return jk
-
-
-class API(Resource):
-
-    def get(self):
-        jk = joke.getJoke()
-        jk = jk.encode('ascii', 'ignore').decode('ascii')
-        # jk = jk.encode('utf-8')
-        return jk
-
-
-api.add_resource(APP, '/')
-api.add_resource(API, '/api')
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
