2a19cebf6206f4eb608a0282a5d8dab04d683997
Will Bennett <william.11bennett@gmail.com>
2017-10-01 20:46:29 -0400

    Updated joke.py
    Added PEP 8 compliance and did some minor additions.

--- joke.py
+++ joke.py
@@ -2,13 +2,23 @@
 import json
 from random import *
 
-def random_digits():
+
+def random_digits(length):
     range_start = 1
-    range_end = 545
-    return randint(range_start, range_end)
-def getJoke():
-    with open('data.json') as data_file:
+    return randint(range_start, length)
+
+
+def getJoke(file='data.json'):
+    """
+    Returns a random joke from file.
+    """
+    with open(file) as data_file:
         data = json.load(data_file)
-    joke = data[random_digits()]
+        l = len(data_file)
+    joke = data[random_digits(l)]
     print joke
     return joke
+
+
+if __name__ == "__main__":
+    getJoke():
