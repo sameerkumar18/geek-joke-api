086220fb9889f676109b481617fdc0954b3ce222
Sameer Kumar <sameer18051998@gmail.com>
2020-03-08 23:51:43 +0530

    Fixed encoding issues, optimized JSON file reads

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
-import joke
-import sys
 from flask_cors import CORS
+
+import joke
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,28 +15,31 @@ class APP(Resource):
 
     def get(self):
         return jsonify({
-		'author' : 'Sameer Kumar',
-		'author_url' : 'https://www.sameerkumar.website',
-		'base_url' : 'https://geek-jokes.sameerkumar.website',
-	    'project_name' : 'Geek Joke API',
-		'project_url' : 'https://github.com/sameerkumar18/geek-joke-api'
-	})
-
+            'author': 'Sameer Kumar',
+            'author_url': 'https://www.sameerkumar.website',
+            'base_url': 'https://geek-jokes.sameerkumar.website',
+            'project_name': 'Geek Joke API',
+            'project_url': 'https://github.com/sameerkumar18/geek-joke-api'
+        })
 
     def post(self):
         jk = joke.get_joke()
-        jk = jk.encode('ascii', 'ignore').decode('ascii')
-        #jk = jk.encode('utf-8')
-        return jk
+        if request.args.get('format') == 'json':
+            return {'joke': jk}
+        else:
+            # Making sure existing clients don't break
+            return jk
 
 
 class API(Resource):
 
     def get(self):
         jk = joke.get_joke()
-        jk = jk.encode('ascii', 'ignore').decode('ascii')
-        # jk = jk.encode('utf-8')
-        return jk
+        if request.args.get('format') == 'json':
+            return {'joke': jk}
+        else:
+            # Making sure existing clients don't break
+            return jk
 
 
 api.add_resource(APP, '/')
